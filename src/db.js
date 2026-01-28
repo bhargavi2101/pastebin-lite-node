7bd2e539167fa0a0b9a14ea2de4c258dbd2ec994
Bhargavi Deotare <deotarebhargavi@gmail.com>
2026-01-28 21:46:07 +0530

    Fix Prisma client for Vercel serverless

--- src/db.js
+++ src/db.js
@@ -1,5 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient();
+}
+
+prisma = global.prisma;
 
 module.exports = prisma;
